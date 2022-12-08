888767b80b43b131a5f5f7903b5895bdfe651209
Mateus de Oliveira Pessoa <mateuspessoadev@gmail.com>
2022-12-08 11:59:44 -0300

    Projeto Finalizado

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.traction.one',
+      },
+    ],
+  },
+}
+
+module.exports = nextConfig
